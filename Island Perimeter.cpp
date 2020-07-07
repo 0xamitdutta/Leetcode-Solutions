5f313ecbaf0ae0493f67304de47d40c19e43ab6d
Amit Dutta <amitdutta2406@gmail.com>
2020-07-07 23:40:03 +0530

    Update Island Perimeter.cpp

--- Island Perimeter.cpp
+++ Island Perimeter.cpp
@@ -4,18 +4,18 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         
-        int count = 0;
+        int islands = 0;
+        int neighbours = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1){
-                    count += 4;
-                    if(i > 0 && grid[i-1][j] == 1)
-                        count -= 2;
-                    if(j > 0 && grid[i][j-1] == 1)
-                        count -= 2;
+                    islands++;
+                    if(i < grid.size()-1 && grid[i+1][j] == 1) neighbours++;    // Down neighbours
+                    if(j < grid[0].size()-1 && grid[i][j+1] == 1) neighbours++; // Right neighbours
+                    // Up and Left neighbours are automatically taken care of cuz we're visiting top-bottom
                 }
             }
         }
-        return count;
+        return islands*4 - neighbours*2;
     }
 };
