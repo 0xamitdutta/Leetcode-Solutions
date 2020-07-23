c06d95d5203c10453a93f6401e12364e928f84f2
Amit Dutta <amitdutta2406@gmail.com>
2020-07-23 11:04:36 +0530

    Update Merge k Sorted Lists.cpp

--- Merge k Sorted Lists.cpp
+++ Merge k Sorted Lists.cpp
@@ -6,6 +6,8 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// TC - O(NlogN), SC - O(N) where N = total number of elements in all linked list
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
@@ -25,5 +27,32 @@ public:
             curr = curr->next;
         }
         return dummy->next;
+    }
+};
+
+// TC - O(Nlogk), SC - O(1) where k = total number of linked list
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, decltype(&comp)> pq(comp);
+        for(auto head : lists){
+            if(head != NULL)
+                pq.push(head);
+        }
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+        while(!pq.empty()){
+            curr->next = pq.top();
+            pq.pop();
+            curr = curr->next;
+            ListNode* next = curr->next;
+            if(next != NULL)
+                pq.push(next);
+        }
+        return dummy->next;
+    }
+    
+    static bool comp(ListNode* p1, ListNode* p2){
+        return p1->val > p2->val;
     }
 };
