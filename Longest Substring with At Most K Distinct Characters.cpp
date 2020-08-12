fbde24e32c5c8c73ddf64e79e06d412803d1267d
Amit Dutta <amitdutta2406@gmail.com>
2020-08-12 12:21:25 +0530

    Update Longest Substring with At Most K Distinct Characters.cpp

--- Longest Substring with At Most K Distinct Characters.cpp
+++ Longest Substring with At Most K Distinct Characters.cpp
@@ -5,19 +5,21 @@ public:
         if(k == 0) return 0;
         
         unordered_map<char, int> m;
+        int res = 0, counter = 0;
         int i = 0, j = 0;
-        int size = 0, counter = 0;
         while(j < s.size()){
             m[s[j]]++;
-            if(m[s[j]] == 1) counter++;
+            if(m[s[j]] == 1) 
+                counter++;
             j++;
             while(counter > k){
                 m[s[i]]--;
-                if(m[s[i]] == 0) counter--;
+                if(m[s[i]] == 0) 
+                    counter--;
                 i++;
             }
-            size = max(size, j-i);
+            res = max(res, j-i);
         }
-        return size;
+        return res;
     }
 };
