b36a33ac770553ca571b4e50bd23e73844edcba7
Amit Dutta <amitdutta2406@gmail.com>
2020-08-16 09:17:21 +0530

    Update Merge Intervals.cpp

--- Merge Intervals.cpp
+++ Merge Intervals.cpp
@@ -6,9 +6,11 @@ public:
         
         sort(nums.begin(), nums.end(), [](vector<int>& a, vector<int>& b){return a[0] < b[0];});
         res.push_back(nums[0]);
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i][0] > res.back()[1]) res.push_back(nums[i]);    //Just think about 2 consecutive elements
-            else res.back()[1] = max(res.back()[1], nums[i][1]);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i][0] > res.back()[1])
+                res.push_back(nums[i]);
+            else 
+                res.back()[1] = max(res.back()[1], nums[i][1]);
         }
         return res;
     }
