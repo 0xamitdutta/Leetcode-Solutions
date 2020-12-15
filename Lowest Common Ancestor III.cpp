467315b87d0a709f57551718f4bb8455e0cb1d6c
Amit Dutta <amitdutta2406@gmail.com>
2020-12-15 16:40:15 +0530

    Create Lowest Common Ancestor III.cpp

---
+++ Lowest Common Ancestor III.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+
+class Solution {
+public:
+    /*
+     * @param root: The root of the binary tree.
+     * @param A: A TreeNode
+     * @param B: A TreeNode
+     * @return: Return the LCA of the two nodes.
+     */
+    bool foundA = false, foundB = false;
+    TreeNode * lowestCommonAncestor3(TreeNode * root, TreeNode * A, TreeNode * B) {
+        auto lca = helper(root, A, B);
+        if(foundA && foundB) 
+            return lca;
+        return NULL;
+    }
+    
+    TreeNode* helper(TreeNode * root, TreeNode * A, TreeNode * B){
+        if(!root) return NULL;
+        
+        auto l = helper(root->left, A, B);
+        auto r = helper(root->right, A, B);
+        
+        if(root == A) {
+            foundA = true;
+            return root;
+        }
+        if(root == B) {
+            foundB = true;
+            return root;
+        }
+        if(l && r) return root;
+        return l != NULL ? l : r;
+    }
+};
