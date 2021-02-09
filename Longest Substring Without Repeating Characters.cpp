d3422123e34ba1999ac58537a6e15c1cf8329fbc
Amit Dutta <amitdutta2406@gmail.com>
2021-02-09 23:46:26 +0530

    Create Longest Substring Without Repeating Characters.cpp

---
+++ Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string str) {
+        if(str.size() == 0) return 0;
+        
+        unordered_map<char, int> hash;
+        int len = 0;
+        for(int i = 0, j = 0; i < str.size(); i++){
+            if(hash.count(str[i])){
+                j = max(j, hash[str[i]]+1);
+            }
+            hash[str[i]] = i;
+            len = max(len, i-j+1);
+        }
+        return len;
+    }
+};
+
+/*
+Keep a hashmap which stores the characters in string as keys and their positions as values, and keep two pointers which define the max substring. move the right pointer to scan through the string , and meanwhile update the hashmap. If the character is already in the hashmap, then move the left pointer to the right of the same character last found. 
+Note that the two pointers can only move forward (especially don't forget that j = max(j, hash[str[i]]) so that j only goes forward).
+*/
