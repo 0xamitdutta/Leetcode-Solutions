9b9b2e67dc2228c27467f1715e50c53c03cc81f0
Amit Dutta <amitdutta2406@gmail.com>
2021-02-12 10:54:50 +0530

    Update Find the Duplicate Number.cpp

--- Find the Duplicate Number.cpp
+++ Find the Duplicate Number.cpp
@@ -1,21 +1,24 @@
-// Try to change the nums array to linked list. The linked list will contain a cycle and the beginning of the loop is the Duplicate
+/* 
+Since the index range in [0, n] and numbers are from [1, n] there is guaranteed to be a duplicate.
+We can take the advantage of the condition: If I use nums[i] as index to get next number as nums[nums[i]] and keep going, eventually, at some point, a duplicated value will be visited again because a duplicate value is mapped to more than one index.
+
+When the first duplicate value is met, from there we start to entering into a cycle where the duplicate value is the entrance of the cycle.
+*/
 
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = 0;
-        int fast = 0;
-        while(true){
+        int slow = nums[0], fast = nums[0];
+        do{
             slow = nums[slow];
             fast = nums[nums[fast]];
-            if(slow == fast){
-                slow = 0;
-                while(slow != fast){
-                    slow = nums[slow];
-                    fast = nums[fast];
-                }
-                return slow;
-            }
+        }while(slow != fast);
+        
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
+        return slow;
     }
 };
