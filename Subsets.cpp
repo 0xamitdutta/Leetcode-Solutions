bfe9443471cdb723f520c3681901697cea8dbc92
Amit Dutta <amitdutta2406@gmail.com>
2021-02-14 12:52:07 +0530

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> curr;
-        backtrack(nums, res, curr, 0);
-        return res;
-    }
-    
-    void backtrack(vector<int>& nums, vector<vector<int>>& res, vector<int>& curr, int index){
-        res.push_back(curr);
-        for(int i = index; i < nums.size(); i++){
-            curr.push_back(nums[i]);
-            backtrack(nums, res, curr, i+1);
-            curr.pop_back();
+        if(nums.size() == 0){
+            return {{}};
         }
+        int num = nums[0];
+        nums.erase(nums.begin());
+        vector<vector<int>> rres = subsets(nums);
+        
+        vector<vector<int>> cres;
+        for(auto& res : rres){
+            cres.push_back(res); // We do not add num 
+            
+            res.insert(res.begin(), num); // We add num to res
+            cres.push_back(res);
+        }
+        return cres;
     }
 };
+
+/*
+Example: 1, 2, 3
+For 2, 3 we have : [], [2], [3], [2, 3];
+
+Then for every ans in 2, 3 we either add or do not add 1
+[], [2], [3], [2, 3]
+[1], [1, 2], [1, 3], [1, 2, 3]
+*/
