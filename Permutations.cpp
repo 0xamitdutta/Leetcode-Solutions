758b817c6d1ad958a83009231fcd63177bd1b2fe
Amit Dutta <amitdutta2406@gmail.com>
2021-02-15 10:21:35 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
+    vector<vector<int>> res;
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> curr;
-        vector<bool> vis(nums.size(), false);
-        permutations(nums, res, curr, vis);
+        helper(nums, vector<int>());
         return res;
     }
     
-    void permutations(vector<int>& nums, vector<vector<int>>& res, vector<int>& curr, vector<bool>& vis){
-        if(curr.size() == nums.size()){
-            res.push_back(curr);
+    void helper(vector<int> nums, vector<int> temp){
+        if(nums.size() == 0){
+            res.push_back(temp);
             return;
         }
         
         for(int i = 0; i < nums.size(); i++){
-            if(vis[i]) continue;
-            vis[i] = true;
-            curr.push_back(nums[i]);
-            permutations(nums, res, curr, vis);
-            curr.pop_back();
-            vis[i] = false;
+            int num = nums[i]; // Take the character at the ith index
+            vector<int> lpart, rpart;   // Find left and right part
+            for(int j = 0; j < i; j++) 
+                lpart.push_back(nums[j]);
+            for(int j = i+1; j < nums.size(); j++)
+                rpart.push_back(nums[j]);
+            
+            // Concatenate left and right part
+            vector<int> rest_of_nums(lpart);
+            rest_of_nums.insert(rest_of_nums.end(), rpart.begin(), rpart.end());
+            
+            // Pass the concatenated and the temp vector by adding current num in temp
+            temp.push_back(num);
+            helper(rest_of_nums, temp);
+            temp.pop_back();
         }
     }
 };
+
+/*
+Example : [1, 2, 3]
+Take 1 pass [2, 3] -> 1,2,3 -> 1,3,2
+Take 2 pass [1, 3] -> 2,1,3 -> 2,3,1
+Take 3 pass [1, 2] -> 3,1,2 -> 3,2,1
+*/
