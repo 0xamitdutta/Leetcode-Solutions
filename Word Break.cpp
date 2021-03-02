03e5ab8428d5ab0c383921ff988fcb241a367c2c
Amit Dutta <amitdutta2406@gmail.com>
2021-03-02 20:18:03 +0530

    Update Word Break.cpp

--- Word Break.cpp
+++ Word Break.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> us(wordDict.begin(), wordDict.end());
-        vector<int> dp(s.size()+1);
-        dp[0] = 1;
-        
+        unordered_set<string> hash(wordDict.begin(), wordDict.end());
         // dp[i] represents whether string ending with char i can be segmented
+        vector<bool> dp(s.size() + 1);
+        
+        dp[0] = true;
         for(int i = 1; i <= s.size(); i++){
             for(int j = 0; j < i; j++){
-                if(dp[j] && us.count(s.substr(j, i-j))){
-                    dp[i] = 1;
-                    break;
+                if(dp[j] && hash.count(s.substr(j, i-j))){
+                    dp[i] = true;
                 }
             }
         }
