9b9028a89cf2bb986a47535ea910867e06b1b422
Amit Dutta <amitdutta2406@gmail.com>
2021-03-13 13:57:32 +0530

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> res(n-k+1);
-        deque<int> deq;
-        // Store index
+        vector<int> res;
+        deque<int> dq;        // Stores index
+        
         for(int i = 0; i < n; i++){
-            // Remove numbers out of range k
-            if(!deq.empty() && deq.front() < i-k+1)
-                deq.pop_front();
-            // Remove smaller numbers in k range as they are useless
-            while(!deq.empty() && nums[i] > nums[deq.back()])
-                deq.pop_back();
-            deq.push_back(i);
-            if(i-k+1 >= 0)
-                res[i-k+1] = nums[deq.front()];
+            // Remove numbers that are out of range k
+            if(!dq.empty() && dq.front() < i-k+1)
+                dq.pop_front();
+            // Remove smaller numbers in within range k as they are useless.
+            // Eg: 10, 20, 30 -> (1) Insert 10 (2) Remove 10, Insert 20, (3) Remove 20, Insert 30. This way maximum element is always at front.
+            while(!dq.empty() && nums[i] > nums[dq.back()])
+                dq.pop_back();
+            
+            dq.push_back(i);
+            if(i-k +1 >= 0)   // Push into result vector if window size >= k. i.e (i+1) - k >= 0
+                res.push_back(nums[dq.front()]);
         }
         return res;
     }
