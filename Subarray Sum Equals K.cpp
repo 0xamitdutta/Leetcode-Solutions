cd847725d86248d2e77ee983b9e8ace83882c2bf
Amit Dutta <amitdutta2406@gmail.com>
2021-03-26 21:27:48 +0530

    Update Subarray Sum Equals K.cpp

--- Subarray Sum Equals K.cpp
+++ Subarray Sum Equals K.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        if(nums.size() == 0)
-            return 0;
+        unordered_map<int, int> hash;
+        hash[0] = 1;
         
-        unordered_map<int, int> m;
-        m[0] = 1;
+        /*
+        .....i...j...k...l
+        Let's say till i sum is x. Till j sum is x+k (Therefore, 1 subarray is from i to j)
+        Let's say till k sum is again x and till l it is x+k. (Therefore, there is subarray from k to l and another from i to l. Hence we have add all ocurrrences of x when sum is x+k)
+        */
         int sum = 0, count = 0;
         for(int& num : nums){
             sum += num;
-            if(m.find(sum-k) != m.end())
-                count += m[sum-k];
-            m[sum] += 1;
+            if(hash.count(sum-k)){
+                count += hash[sum-k];
+            }
+            hash[sum] += 1;
         }
         return count;
     }
