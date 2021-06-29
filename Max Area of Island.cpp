38410446a3602495fa65869daee72cd26f2c54f6
Amit Dutta <amitdutta2406@gmail.com>
2021-06-30 01:34:40 +0530

    Update Max Area of Island.cpp

--- Max Area of Island.cpp
+++ Max Area of Island.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m = grid.size();
-        if(m == 0)
-            return 0;
-        int n = grid[0].size();
+        int m = grid.size(), n = grid[0].size();
         
-        int max_count = 0;
+        int max_area = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                int curr = 0;
                 if(grid[i][j] == 1){
-                    dfs(grid, i, j, curr);
-                    max_count = max(max_count, curr);
+                    max_area = max(max_area, dfs(grid, i, j));
                 }
             }
         }
-        return max_count;
+        return max_area;
     }
     
-    void dfs(vector<vector<int>>& grid, int i, int j, int& curr){
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0)
-            return;
+    int dfs(vector<vector<int>>& grid, int i, int j){
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) return 0;
         
-        curr += 1;
         grid[i][j] = 0;
-        dfs(grid, i-1, j, curr);
-        dfs(grid, i, j+1, curr);
-        dfs(grid, i+1, j, curr);
-        dfs(grid, i, j-1, curr);
+        int area = 1;
+        area += dfs(grid, i-1, j);
+        area += dfs(grid, i, j+1);
+        area += dfs(grid, i+1, j);
+        area += dfs(grid, i, j-1);
+        return area;
     }
 };
