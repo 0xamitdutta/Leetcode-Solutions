87a47ed96e0543cbbd18531c1aceede222b487a6
Amit Dutta <amitdutta2406@gmail.com>
2021-07-12 18:41:13 +0530

    Update Find Median from Data Stream.cpp

--- Find Median from Data Stream.cpp
+++ Find Median from Data Stream.cpp
@@ -1,27 +1,32 @@
-class MedianFinder {
-public:
-    /* Use 2 heaps.
+/*  
+    Use 2 heaps.
     One max heap for storing smaller half elements.
     Another min heap for storing larger half elements.
-    */
-    priority_queue<long> small, large; // long is used cuz problem will arise when we try to negate -2^31. Range of int is (-2^31 to 2^31 - 1)
-    MedianFinder() {
-    }
-    
-    void addNum(int num) {
-        // Pay attention xD
-        small.push(num);
-        large.push(-small.top()); // Min heap is made by negating the integers
-        small.pop();
-        if(small.size() < large.size()){
-            small.push(-large.top());
+*/
+class MedianFinder {
+private:
+    priority_queue<int> small; // smaleer half which is a max_heap
+    priority_queue<int, vector<int>, greater<int>> large; // larger half which is a min_heap
+    bool even = true;
+public:
+    void addNum(int num) { // O(logn)
+        if(even) {
+            large.push(num);
+            small.push(large.top());
             large.pop();
         }
+        else {
+            small.push(num);
+            large.push(small.top());
+            small.pop();
+        }
+        even = !even;
     }
     
-    double findMedian() {
-        // If odd elements return top of small else return mean of top of small and top of large
-        return small.size() > large.size() ? small.top() : (small.top()-large.top())/2.0;
+    double findMedian() { // O(1)
+        if(even) 
+            return (small.top() + large.top()) / 2.0;
+        return small.top();
     }
 };
 
