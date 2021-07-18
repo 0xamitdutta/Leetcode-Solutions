7bc4c4f1a23b2b8bd5b991bd10ec65ef98928188
Amit Dutta <amitdutta2406@gmail.com>
2021-07-18 16:29:35 +0530

    Update K Closest Points to Origin.cpp

--- K Closest Points to Origin.cpp
+++ K Closest Points to Origin.cpp
@@ -1,3 +1,17 @@
+// O(nlogn)
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        sort(points.begin(), points.end(), comp);
+        return vector<vector<int>>(points.begin(), points.begin()+k);
+    }
+    
+    static bool comp(vector<int>& a, vector<int>& b) {
+        return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1]*b[1];
+    } 
+};
+
+// O(nlogk)
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
