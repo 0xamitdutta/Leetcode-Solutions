cccc162be33489373bbec1166d68163aeec9eddb
Amit Dutta <amitdutta2406@gmail.com>
2021-07-18 16:26:13 +0530

    Update K Closest Points to Origin.cpp

--- K Closest Points to Origin.cpp
+++ K Closest Points to Origin.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        priority_queue<pair<int, pair<int, int>> > pq;
-        for(auto point : points){
-            int x = point[0];
-            int y = point[1];
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>> pq;
+        for(auto point : points) {
+            int x = point[0], y = point[1]; 
             int d = x*x + y*y;
-            pq.push({d, {x, y}});
-            if(pq.size() > K)
+            pq.push({d, x, y});
+            if(pq.size() > k)
                 pq.pop();
         }
-        
         vector<vector<int>> res;
-        while(!pq.empty()){
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
-            res.push_back({x, y});
+        while(!pq.empty()) {
+            res.push_back({pq.top()[1], pq.top()[2]});
             pq.pop();
         }
         return res;
