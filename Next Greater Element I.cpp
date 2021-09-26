470fba3b2b1030d3e2a957945924894ab3fe200d
Amit Dutta <amitdutta2406@gmail.com>
2021-09-27 00:06:26 +0530

    Update Next Greater Element I.cpp

--- Next Greater Element I.cpp
+++ Next Greater Element I.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // We are tracking the next greater element of i using a hashmap. Thus hash[i] = j means j is the next greater element for i.
         unordered_map<int, int> m;
         stack<int> s;
         for(int i = 0; i < nums2.size(); i++){
-            while(!s.empty() && nums2[s.top()] < nums2[i]){
-                m[nums2[s.top()]] = nums2[i];
+            while(!s.empty() && nums2[i] > s.top()){
+                m[s.top()] = nums2[i];
                 s.pop();
             }
-            s.push(i);
+            s.push(nums2[i]);
         }
+        
         for(int i = 0; i < nums1.size(); i++){
             if(m.find(nums1[i]) != m.end())
                 nums1[i] = m[nums1[i]];
