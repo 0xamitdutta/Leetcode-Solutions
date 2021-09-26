1dd028259e3e5743e745f61f24a05999ef48005c
Amit Dutta <amitdutta2406@gmail.com>
2021-09-27 00:25:54 +0530

    Update Next Greater Element II.cpp

--- Next Greater Element II.cpp
+++ Next Greater Element II.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        vector<int> v(n, -1);
+        vector<int> A;
+        // Best way for handling circular array is to create new array and store all elements twice
+        for(int i = 0; i < 2*n; i++) A.push_back(nums[i % n]);
+        
+        // Stack is storing the indexes instead of values which is helpful
         stack<int> s;
-        for(int i = 0; i < 2*n; i++){
-            while(!s.empty() && nums[s.top()] < nums[i%n]){
-                v[s.top()] = nums[i%n];
+        vector<int> res(n, -1);
+        for(int i = 0; i < A.size(); i++) {
+            while(!s.empty() && A[i] > A[s.top()]) {
+                res[s.top()] = A[i];
                 s.pop();
             }
             if(i < n)
                 s.push(i);
         }
-        return v;
+        return res;
     }
 };
