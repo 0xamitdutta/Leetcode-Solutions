4d95510105f1de669d2205970ff7e202c3793a12
Amit Dutta <amitdutta2406@gmail.com>
2021-12-12 11:15:57 +0530

    Update Lowest Common Ancestor II.cpp

--- Lowest Common Ancestor II.cpp
+++ Lowest Common Ancestor II.cpp
@@ -18,9 +18,7 @@ public:
      * @param B: node in the tree
      * @return: The lowest common ancestor of A and B
      */
-    ParentTreeNode * lowestCommonAncestorII(ParentTreeNode * root, ParentTreeNode * A, ParentTreeNode * B) {
-        if(!root || !A || !B) return NULL;
-        
+    ParentTreeNode * lowestCommonAncestorII(ParentTreeNode * A, ParentTreeNode * B) {
         unordered_set<ParentTreeNode*> s;
         ParentTreeNode* node = A;
         while(node != NULL){
@@ -34,5 +32,24 @@ public:
             node = node->parent;
         }
         return node;
+    }
+};
+
+
+class Solution {
+public:
+    /*
+     * @param root: The root of the tree
+     * @param A: node in the tree
+     * @param B: node in the tree
+     * @return: The lowest common ancestor of A and B
+     */
+    ParentTreeNode * lowestCommonAncestorII(ParentTreeNode * A, ParentTreeNode * B) {
+        ParentTreeNode* a = A, *b = B;
+        while(a != b) {
+            a = a == NULL ? B : a->parent;
+            b = b == NULL ? A : b->parent;
+        }
+        return a;
     }
 };
