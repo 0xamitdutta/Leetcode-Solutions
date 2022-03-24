7beb3fb23bcdc02cbc9f121e7b3c6d7105104ce4
Amit Dutta <amitdutta2406@gmail.com>
2022-03-24 23:26:56 +0530

    Update Shortest Bridge.cpp

--- Shortest Bridge.cpp
+++ Shortest Bridge.cpp
@@ -1,68 +1,57 @@
 class Solution {
 public:
-    int shortestBridge(vector<vector<int>>& A) {
-        int m = A.size();
-        int n = A[0].size();
-        bool isColored = false;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(A[i][j] == 1){
-                    dfs(A, i, j);
-                    isColored = true;
-                    break;
+    vector<pair<int, int>> dirs = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    queue<pair<int, int>> q;
+    int vis[100][100] = {};
+    int shortestBridge(vector<vector<int>>& grid) {
+        int n = grid.size();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    dfs(grid, i, j);
+                    return bfs(grid);
                 }
             }
-            if(isColored){
-                break;
-            }
-        }
-        
-        queue<pair<int, int>> q;
-        vector<vector<int>> vis(m, vector<int>(n));
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(A[i][j] == 2){
-                    q.push({i, j});
-                    vis[i][j] = 1;
-                }
-            }
-        }
-        int flips = 0;
-        vector<int> xdirs = {0, 0, 1, -1};
-        vector<int> ydirs = {1, -1, 0, 0};
-        
-        while(!q.empty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                int x = q.front().first;
-                int y = q.front().second;
-                q.pop();
-                
-                if(A[x][y] == 1){
-                    return flips-1;
-                }
-                for(int j = 0; j < 4; j++){
-                    int new_x = x + xdirs[j];
-                    int new_y = y + ydirs[j];
-                    if(new_x >= 0 && new_x < m && new_y >= 0 && new_y < n && !vis[new_x][new_y]){
-                        q.push({new_x, new_y});
-                        vis[new_x][new_y] = 1;
-                    }
-                }
-            }
-            flips++;
         }
         return -1;
     }
     
-    void dfs(vector<vector<int>>& A, int i, int j){
-        if(i < 0 || i >= A.size() || j < 0 || j >= A[0].size() || A[i][j] != 1){
+    bool invalid(vector<vector<int>>& grid, int i, int j) {
+        if(i < 0 || j < 0 || i == grid.size() || j == grid[0].size())
+            return true;
+        return false;
+    }
+    
+    void dfs(vector<vector<int>>& grid, int x, int y) {
+        if(invalid(grid, x, y) || grid[x][y] == 0 || vis[x][y])
             return;
+        vis[x][y] = 1;
+        q.push({x, y});
+        for(auto dir : dirs) {
+            int nx = x + dir.first, ny = y + dir.second;
+            dfs(grid, nx, ny);
         }
-        A[i][j] = 2;
-        dfs(A, i+1, j);
-        dfs(A, i-1, j);
-        dfs(A, i, j+1);
-        dfs(A, i, j-1);
+    }
+        
+    int bfs(vector<vector<int>>& grid) {
+        int res = 0;
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i = 0; i < n; i++) {
+                int x = q.front().first, y = q.front().second;
+                q.pop();
+                for(auto dir : dirs) {
+                    int nx = x + dir.first, ny = y + dir.second;
+                    if(invalid(grid, nx, ny) || vis[nx][ny])
+                        continue;
+                    if(grid[nx][ny] == 1)
+                        return res;
+                    vis[nx][ny] = 1;
+                    q.push({nx, ny});
+                }
+            }
+            res++;
+        }
+        return -1;
     }
 };
