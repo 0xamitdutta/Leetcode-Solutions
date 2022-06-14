461ab4d84a70debb97c82ccfc65aff9bd4087c98
Amit Dutta <amitdutta2406@gmail.com>
2022-06-14 13:10:57 +0530

    Update Surrounded Regions.cpp

--- Surrounded Regions.cpp
+++ Surrounded Regions.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        if(m == 0)
-            return;
-        int n = board[0].size();
-        
-        for(int i = 0; i < m; i++){
-            if(board[i][0] == 'O') dfs(board, i, 0);
-            if(board[i][n-1] == 'O') dfs(board, i, n-1);
+    void solve(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        for(int i = 0; i < m; i++) {
+            if(grid[i][0] == 'O') dfs(grid, i, 0);
+            if(grid[i][n-1] == 'O') dfs(grid, i, n-1);
         }
-        for(int j = 0; j < n; j++){
-            if(board[0][j] == 'O') dfs(board, 0, j);
-            if(board[m-1][j] == 'O') dfs(board, m-1, j);
+        for(int j = 0; j < n; j++) {
+            if(grid[0][j] == 'O') dfs(grid, 0, j);
+            if(grid[m-1][j] == 'O') dfs(grid, m-1, j);
         }
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(board[i][j] == 'O')
-                    board[i][j] = 'X';
-                else if(board[i][j] == '*')
-                    board[i][j] = 'O';
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 'O') grid[i][j] = 'X';
+                if(grid[i][j] == '.') grid[i][j] = 'O';
             }
         }
-        return;
     }
     
-    void dfs(vector<vector<char>>& board, int i, int j){
-        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] == '*' || board[i][j] == 'X')
-            return;
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != 'O') return;
         
-        board[i][j] = '*';
-        dfs(board, i-1, j);
-        dfs(board, i, j+1);
-        dfs(board, i+1, j);
-        dfs(board, i, j-1);
+        grid[i][j] = '.';
+        dfs(grid, i+1, j); dfs(grid, i-1, j); dfs(grid, i, j+1); dfs(grid, i, j-1);
     }
 };
