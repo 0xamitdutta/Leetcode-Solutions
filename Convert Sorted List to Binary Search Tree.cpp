b55ba7423c1c0fb35ea918154dfa6c31f5112a32
Amit Dutta <amitdutta2406@gmail.com>
2022-06-25 01:16:28 +0530

    Update Convert Sorted List to Binary Search Tree.cpp

--- Convert Sorted List to Binary Search Tree.cpp
+++ Convert Sorted List to Binary Search Tree.cpp
@@ -3,7 +3,9 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 /**
@@ -12,31 +14,55 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ 
+// TC = O(nlogn)
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-        if(head == NULL) return NULL;
-        
-        vector<int> v;
-        ListNode *curr = head;
-        while(curr != NULL){
-            v.push_back(curr->val);
-            curr = curr->next;
+        if(!head) return NULL;
+        if(!head->next) {
+            TreeNode* node = new TreeNode(head->val);
+            return node;
         }
-        return constructBST(v, 0, v.size()-1);
+        
+        ListNode* prev, *slow = head, *fast = head;
+        while(slow && fast && fast->next) {
+            prev = slow; 
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = NULL;
+        TreeNode* node = new TreeNode(slow->val);
+        node->left = sortedListToBST(head);
+        node->right = sortedListToBST(slow->next);
+        return node;
+    }
+};
+ 
+// TC = O(n)
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> nums;
+        while(head != NULL) {
+            nums.push_back(head->val);
+            head = head->next;
+        }
+        return sortedListToBSTHelper(nums, 0, nums.size()-1);
     }
     
-    TreeNode* constructBST(vector<int>& v, int low, int high){
-        if(low > high)return NULL;
+    TreeNode* sortedListToBSTHelper(vector<int>& nums, int low, int high) {
+        if(low > high) return NULL;
         
-        int mid = low + (high-low)/2;
-        TreeNode* root = new TreeNode(v[mid]);
-        
-        root->left = constructBST(v, low, mid-1);
-        root->right = constructBST(v, mid+1, high);
-        return root;
+        int mid = (low + high)/2;
+        TreeNode* node = new TreeNode(nums[mid]);
+        node->left = sortedListToBSTHelper(nums, low, mid-1);
+        node->right = sortedListToBSTHelper(nums, mid+1, high);
+        return node;
     }
 };
