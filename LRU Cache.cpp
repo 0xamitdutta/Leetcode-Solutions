7e7318bd25332eca648abc3edc8559a56d387513
Amit Dutta <amitdutta2406@gmail.com>
2022-06-28 16:38:10 +0530

    Update LRU Cache.cpp

--- LRU Cache.cpp
+++ LRU Cache.cpp
@@ -75,3 +75,81 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+    int val;
+    ListNode *next, *prev;
+    
+    ListNode(int x) : val(x), next(NULL), prev(NULL) {}
+};
+
+class LRUCache {
+private:
+    int size, cap;
+    ListNode *head = new ListNode(0), *tail = new ListNode(0);
+    
+    void add(ListNode* node) {
+        ListNode *next = head->next;
+        head->next = node;
+        node->next = next;
+        node->prev = head;
+        next->prev = node;
+    }
+    
+    void update (ListNode* node) {
+        remove(node);
+        add(node);
+    }
+    
+    void remove (ListNode* node) {
+        ListNode *prev = node->prev, *next = node->next;
+        prev->next = next;
+        next->prev = prev;
+    }
+    
+    void pop_back() {
+        ListNode *node = tail->prev;
+        remove(node);
+        m.erase(node->val);
+        delete node;
+    }
+public:
+    LRUCache(int capacity) {
+        size = 0;
+        cap = capacity;
+    }
+    
+    /* Check 2 things here
+    1. If value exists update node
+    2. If value doesn't exist add node but check size
+    */
+    int getValue(int key) {
+        if(m.count(key)) {
+            update(m[key]);
+            return m[key]->val;
+        } 
+        addKey(key);
+        return m[key]->val;
+    }
+    
+    void addKey(int key) {
+        ListNode *node = new ListNode(key);
+        m[key] = node;
+        add(node);
+        size++;
+        if(size > cap) {
+            pop_back();
+            size--;
+        }
+    }
+};
+
+int main() {
+    LRUCache *lru = new LRUCache(5);
+    int a = lru->getValue(3);
+    return 0;
+}
+
