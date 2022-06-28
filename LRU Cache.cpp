60be22aefe2b3bbcc85cc6b5b969c3f0e2736c74
Amit Dutta <amitdutta2406@gmail.com>
2022-06-28 16:16:40 +0530

    Update LRU Cache.cpp

--- LRU Cache.cpp
+++ LRU Cache.cpp
@@ -1,28 +1,77 @@
-class LRUCache {
-    list<int> l;
-    unordered_map<int, list<int>::iterator> hash;
-    int size;
-    public:
-        LRUCache(int n);
-        void addPage(int x);
+class Node {
+public:
+    int key, val;
+    Node *next, *prev;
+    
+    Node(int x, int y) : key(x), val(y), prev(NULL), next(NULL) {}
 };
 
-LRUCache::LRUCache(int n) {
-    size = n;
-}
-
-void LRUCache::addPage(int x) {
-    if(!hash.count(x)) {
-        if(l.size() == size) {
-            int last = l.back(); l.pop_back();
-            hash.erase(last);
-        }
-        l.push_front(x);
-        hash[x] = l.begin();
-        return;
+class LRUCache {
+private:
+    int size, cap;
+    Node *head = new Node(0, 0), *tail = new Node(0, 0);
+    map<int, Node*> m;
+    
+    void add(Node* node) {
+        Node *next = head->next;
+        head->next = node;
+        node->next = next;
+        node->prev = head;
+        next->prev = node;
     }
+    
+    void update(Node* node) {
+        remove(node);
+        add(node);
+    }
+    
+    void remove(Node* node) {
+        Node *prev = node->prev, *next = node->next;
+        prev->next = next;
+        next->prev = prev;
+    }
+    
+    void pop_back() {
+        Node* node = tail->prev;
+        remove(node);
+        m.erase(node->key);
+        delete node;
+    }
+public:
+    LRUCache(int capacity) {
+        size = 0;
+        cap = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if(!m.count(key)) 
+            return -1;
+        update(m[key]); // Update its position and bring it at front
+        return m[key]->val;
+    }
+    
+    void put(int key, int value) {
+        if(m.count(key)) {
+            m[key]->val = value;
+            update(m[key]);
+        } else {
+            Node *node = new Node(key, value);
+            m[key] = node;
+            add(node);
+            size++;
+            if(size > cap) {
+                pop_back();
+                size--;
+            }
+        }
+    }
+};
 
-    l.erase(hash[x]);
-    l.push_front(x);
-    hash[x] = l.begin();
-}
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
