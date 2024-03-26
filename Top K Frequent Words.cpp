24cc59708aa656776392579739a539daf9bf590d
Amit Dutta <amitdutta2406@gmail.com>
2024-03-26 12:23:37 +0530

    Update Top K Frequent Words.cpp

--- Top K Frequent Words.cpp
+++ Top K Frequent Words.cpp
@@ -3,20 +3,19 @@
  https://leetcode.com/problems/top-k-frequent-words/discuss/1274279/CPP-or-C%2B%2B-or-priority-queue-or-Custom-Comparator-and-Solution-Explain-oror-O(nLogk)-Solution
 */
 
+#define PIS pair<int, string> 
+
 class Solution {
 public:
-    vector<string> topKFrequent(vector<string>& words, int K) {
-        map<string, int> m;
-        for(auto word : words) m[word]++;
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> m;
+        for(string word : words) 
+            m[word]++;
         
-        auto comp = [](pair<int, string>& a, pair<int, string>& b) {
-            if(a.first == b.first) return a.second < b.second;
-            return a.first > b.first;
-        };
-        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(comp)> pq(comp);
-        for(auto [key, val] : m) {
-            pq.push({val, key});
-            if(pq.size() > K)
+        priority_queue<PIS, vector<PIS>, Comparator> pq;
+        for(auto [key, value] : m) {
+            pq.push({value, key});
+            if(pq.size() > k)
                 pq.pop();
         }
         vector<string> res;
@@ -27,4 +26,18 @@ public:
         reverse(res.begin(), res.end());
         return res;
     }
+
+    struct Comparator {
+        /* 
+            The comparison is done from bottom to top.
+            a.first > b.first means the higher frequency will be at bottom
+            a.second < b.second means the lexicographically smaller string will be at bottom
+        */
+        bool operator()(const PIS& a,const PIS& b) {
+            if(a.first == b.first) 
+                return a.second < b.second;
+            return a.first > b.first;
+        }
+    };
 };
+
