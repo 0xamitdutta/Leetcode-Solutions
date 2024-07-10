9f58a571642be50945baceb78f59c8174c2b4888
Amit Dutta <amitdutta2406@gmail.com>
2024-07-10 15:25:22 +0530

    Update Spiral Matrix II.cpp

--- Spiral Matrix II.cpp
+++ Spiral Matrix II.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> grid(n, vector<int>(n));
-        int r1 = 0;
-        int r2 = n-1;
-        int c1 = 0;
-        int c2 = n-1;
-        int num = 1;
-        
-        while(r1 <= r2 && c1 <= c2){
-            for(int j = c1; j <= c2; j++)
-                grid[r1][j] = num++;
+        vector<vector<int>> res(n, vector<int>(n, 0));
+        int r1 = 0, r2 = n-1, c1 = 0, c2 = n-1;
+
+        int val = 1;
+        while(r1 <= r2 && c1 <= c2) {
+            // Right
+            for(int i = c1; i <= c2; i++)
+                res[r1][i] = val++;
             r1++;
-            
-            for(int i = r1; i <= r2; i++)
-                grid[i][c2] = num++;
-            c2--; 
-            
-            if(r1 <= r2){
-                for(int j = c2; j >= c1; j--)
-                    grid[r2][j] = num++;
-                r2--; 
-            }
-            
-            if(c1 <= c2){
-                for(int i = r2; i >= r1; i--)
-                    grid[i][c1] = num++;
-                c1++; 
-            }
+            // Down
+            for(int i = r1; i <= r2; i++) 
+                res[i][c2] = val++;
+            c2--;
+
+            if(r1 > r2 || c1 > c2) return res;
+
+            // Left
+            for(int i = c2; i >= c1; i--) 
+                res[r2][i] = val++;
+            r2--;
+
+            // Top
+            for(int i = r2; i >= r1; i--)
+                res[i][c1] = val++;
+            c1++;
         }
-        return grid;
+        return res;
     }
 };
