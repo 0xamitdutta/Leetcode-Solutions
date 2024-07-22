4b32b21d2cd19207208b0245a76f111c687e18be
Amit Dutta <amitdutta2406@gmail.com>
2024-07-23 03:36:16 +0530

    Update Number of Provinces.cpp

--- Number of Provinces.cpp
+++ Number of Provinces.cpp
@@ -34,3 +34,40 @@ public:
         }
     }
 };
+
+// Union Find
+class Solution {
+public:
+    vector<int> par;
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        par.resize(n, -1);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(isConnected[i][j] && i != j)
+                    doUnion(i, j);
+            }
+        }
+
+        unordered_set<int> uniqueParents;
+        for(int i = 0; i < n; i++) {
+            int parI = find(i);
+            uniqueParents.insert(parI);
+        }
+        return uniqueParents.size();
+    }
+
+    void doUnion(int u, int v) {
+        int parU = find(u);
+        int parV = find(v);
+        if(parU != parV)
+            par[parV] = parU;
+    }
+
+    int find(int node) {
+        if(par[node] == -1)
+            return node;
+        return par[node] = find(par[node]);
+    }
+};
