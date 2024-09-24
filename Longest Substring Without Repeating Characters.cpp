757c9148ddef527de7f5f4e76e714c478731e978
Amit Dutta <amitdutta2406@gmail.com>
2024-09-25 02:43:19 +0530

    Update Longest Substring Without Repeating Characters.cpp

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -17,7 +17,8 @@ public:
 };
 
 /*
-Keep a hashmap which stores the characters in string as keys and their positions as values, and keep two pointers which define the max substring. move the right pointer to scan through the string , and meanwhile update the hashmap. If the character is already in the hashmap, then move the left pointer to the right of the same character last found. 
+Keep a hashmap which stores the characters in string as keys and their positions as values, and keep two pointers which define the max substring. move the right pointer to scan through the string , and meanwhile update the hashmap. 
+If the character is already in the hashmap, then move the left pointer to the right of the same character last found. 
 Note that the two pointers can only move forward (especially don't forget that j = max(j, hash[str[i]]) so that j only goes forward).
 */
 class Solution {
