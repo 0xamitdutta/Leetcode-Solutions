034cea740384e6f873f9efdc92adde231c86fb81
Amit Dutta <amitdutta2406@gmail.com>
2024-09-27 03:04:44 +0530

    Update Course Schedule.cpp

--- Course Schedule.cpp
+++ Course Schedule.cpp
@@ -3,8 +3,8 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& pre) {
         // The BFS version of Topological sort ensures that there's at least 1 vertex with indegree 0 and outdegree 0.
         vector<int> indegree(numCourses);
-        for(int i = 0; i < pre.size(); i++)
-            indegree[pre[i][0]]++;
+        for(vector<int>& prereq : prerequisites) 
+            indegree[prereq[0]]++;
         
         // Put all nodes with indegree 0 in the queue
         queue<int> q;
@@ -14,19 +14,18 @@ public:
         }
         
         // Check if count is equal to numCourses
-        int count = 0;
-        while(!q.empty()){
-            count++;
-            int curr = q.front();
+        while(!q.empty()) {
+            int course = q.front();
             q.pop();
-            for(int i = 0; i < pre.size(); i++){
-                if(pre[i][1] == curr){
-                    indegree[pre[i][0]]--;
-                    if(indegree[pre[i][0]] == 0)
-                        q.push(pre[i][0]);
+            numCourses--;
+            for(vector<int>& prereq : prerequisites) {
+                if(prereq[1] == course) {
+                    indegree[prereq[0]]--;
+                    if(indegree[prereq[0]] == 0)
+                        q.push(prereq[0]);
                 }
             }
         }
-        return count == numCourses;
+        return numCourses == 0;
     }
 };
