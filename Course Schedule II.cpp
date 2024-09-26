ac73f83ce072df3ebbb94fd5c6e3ba833955959a
Amit Dutta <amitdutta2406@gmail.com>
2024-09-27 03:09:00 +0530

    Update Course Schedule II.cpp

--- Course Schedule II.cpp
+++ Course Schedule II.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
         vector<int> indegree(numCourses);
-        for(int i = 0; i < pre.size(); i++)
-            indegree[pre[i][0]]++;
+        for(vector<int>& prereq : prerequisites) 
+            indegree[prereq[0]]++;
         
+        // Put all nodes with indegree 0 in the queue
         queue<int> q;
         for(int i = 0; i < indegree.size(); i++){
-            if(indegree[i] == 0)
+            if(!indegree[i])
                 q.push(i);
         }
+
         vector<int> res;
-        while(!q.empty()){
-            int temp = q.front();
+        while(!q.empty()) {
+            int course = q.front();
             q.pop();
-            res.push_back(temp);
-            for(int i = 0; i < pre.size(); i++){
-                if(pre[i][1] == temp){
-                    indegree[pre[i][0]]--;
-                    if(indegree[pre[i][0]] == 0)
-                        q.push(pre[i][0]);
+            numCourses--;
+            res.push_back(course);
+            for(vector<int>& prereq : prerequisites) {
+                if(prereq[1] == course) {
+                    indegree[prereq[0]]--;
+                    if(indegree[prereq[0]] == 0)
+                        q.push(prereq[0]);
                 }
             }
         }
-        return res.size() == numCourses ? res : vector<int>();
+        return numCourses == 0 ? res : vector<int>();
     }
 };
