186eaeeb0973a5707cc0db5b48ecf7b5249c0ae2
Amit Dutta <amitdutta2406@gmail.com>
2024-09-27 02:24:54 +0530

    Update Alien Dictionary (Easy).cpp

--- Alien Dictionary (Easy).cpp
+++ Alien Dictionary (Easy).cpp
@@ -1,22 +1,27 @@
-/* https://www.lintcode.com/problem/alien-dictionaryeasy/description */
+/* https://leetcode.com/problems/verifying-an-alien-dictionary/description/ */
 class Solution {
 public:
     bool isAlienSorted(vector<string> &words, string &order) {
-        if(words.size() == 0 || words.size() == 1) return words.size();
-        
-        unordered_map<char, int> m;
+        unordered_map<char, int> hash_map;
         for(int i = 0; i < order.size(); i++)
-            m[order[i]] = i+1;
-        for(int i = 0; i < words.size()-1; i++){
+            hash_map[order[i]] = i;
+        for(int i = 0; i < words.size()-1; i++) {
             string word1 = words[i];
             string word2 = words[i+1];
-            int len = min(word1.size(), word2.size());
-            for(int j = 0; j < len; j++){
-                if(m[word1[j]] < m[word2[j]]) break;
-                else if(m[word1[j]] == m[word2[j]]) continue;
-                else return false;
-            }
+            if(!isValid(word1, word2, hash_map))
+                return false;
         }
         return true;
+    }
+
+    bool isValid(string& word1, string& word2, unordered_map<char, int>& hash_map) {
+        int m = word1.size(), n = word2.size();
+        for(int i = 0; i < min(m, n); i++) {
+            int pos1 = hash_map[word1[i]], pos2 = hash_map[word2[i]];
+            if(pos1 == pos2)
+                continue;
+            return pos1 < pos2 ? true : false;
+        }
+        return m <= n ? true : false;
     }
 };
