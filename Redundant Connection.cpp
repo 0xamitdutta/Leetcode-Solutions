0b6ecb8b2738d3b3ff0f1ec53456c60c5e0e2c9c
Amit Dutta <amitdutta2406@gmail.com>
2024-11-15 18:02:38 +0530

    Update Redundant Connection.cpp

--- Redundant Connection.cpp
+++ Redundant Connection.cpp
@@ -37,7 +37,7 @@ public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
         par.resize(n+1); size.resize(n+1);
-        for(int i = 0; i < par.size(); i++) {
+        for(int i = 1; i <= n; i++) {
             par[i] = i; size[i] = 1;
         }
 
