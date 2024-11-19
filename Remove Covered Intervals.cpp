bf2e28b52093609f1a3520f7c40361387455ca62
Amit Dutta <amitdutta2406@gmail.com>
2024-11-19 12:55:13 +0530

    Update Remove Covered Intervals.cpp

--- Remove Covered Intervals.cpp
+++ Remove Covered Intervals.cpp
@@ -20,25 +20,24 @@ public:
 };
 
 // O(nlogn)
+// First sort by starting time. If 2 intervals have the same start time then the interval whose ending time is greater should come first
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        sort(intervals.begin(), intervals.end(), comp);
-        
-        vector<vector<int>> res;
-        res.push_back(intervals[0]);
-        for(int i = 1; i < n; i++) {
-            if(res.back()[0] <= intervals[i][0] && res.back()[1] >= intervals[i][1])
-                continue;
-            res.push_back(intervals[i]);
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b){
+            if(a[0] == b[0])
+                return a[1] > b[1];
+            return a[0] < b[0];
+        });
+        int count = 0;
+        vector<int> currentInterval = intervals[0];
+        for(int i = 1; i < intervals.size(); i++) {
+            vector<int> interval = intervals[i];
+            if(currentInterval[0] <= interval[0] && currentInterval[1] >= interval[1])
+                count++;
+            else
+                currentInterval = interval;
         }
-        return res.size();
-    }
-    
-    static bool comp(vector<int>& a, vector<int>& b) {
-        if(a[0] == b[0]) 
-            return a[1] > b[1];
-        return a[0] < b[0];
+        return intervals.size() - count;
     }
 };
