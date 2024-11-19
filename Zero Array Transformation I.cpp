8e0b358032efc0ca0b3d2f0603fe66aa33f23aff
Amit Dutta <amitdutta2406@gmail.com>
2024-11-19 18:32:33 +0530

    Create Zero Array Transformation I.cpp

---
+++ Zero Array Transformation I.cpp
@@ -0,0 +1,19 @@
+// Line Sweep Algorithm using prefix sum
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<int> prefix(n + 1);
+        for(auto& query : queries) {
+            prefix[query[0]]--;
+            prefix[query[1] + 1]++;
+        }
+        for(int i = 1; i < n; i++) 
+            prefix[i] += prefix[i-1];
+        for(int i = 0; i < n; i++) {
+            if(nums[i] + prefix[i] > 0)
+                return false;
+        }
+        return true;
+    }
+};
