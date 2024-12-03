1d348394d88ef1b6c043a132d6be66342a108001
Amit Dutta <amitdutta2406@gmail.com>
2024-12-03 17:51:05 +0530

    Update Valid Palindrome II.cpp

--- Valid Palindrome II.cpp
+++ Valid Palindrome II.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     bool validPalindrome(string s) {
         int i = 0, j = s.size()-1;
-        int count = 0;
-        while(i < j){
-            if(s[i] == s[j]){
-                i++; j--;
-            }
-            else{
+        while(i < j) {
+            if(s[i] != s[j])
                 return isValid(s, i+1, j) || isValid(s, i, j-1);
+            else {
+                i++; j--;
             }
         }
         return true;
     }
     
-    bool isValid(string& s, int i, int j){
-        while(i < j){
-            if(s[i] == s[j]){
+    bool isValid(string& s, int i, int j) {
+        while(i < j) {
+            if(s[i] != s[j])
+                return false;
+            else {
                 i++; j--;
             }
-            else
-                return false;
         }
         return true;
     }
