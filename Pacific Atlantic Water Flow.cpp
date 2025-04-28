9fd66d3018a72850e4a35e271e9c5919c12c6ee0
Amit Dutta <amitdutta2406@gmail.com>
2025-04-29 02:12:48 +0530

    Update Pacific Atlantic Water Flow.cpp

--- Pacific Atlantic Water Flow.cpp
+++ Pacific Atlantic Water Flow.cpp
@@ -45,6 +45,48 @@ public:
         dfs(matrix, i-1, j, matrix[i][j], ocean);
         dfs(matrix, i+1, j, matrix[i][j], ocean);
         dfs(matrix, i, j+1, matrix[i][j], ocean);
-        dfs(matrix, i, j-1, matrix[i][j], ocean);
+        dfs(matrix, i, j-1, matrix[i][j], ocean);        
+    }
+};
+
+
+// BFS Solution
+class Solution {
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int m = heights.size(), n = heights[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> aOcean(m, vector<int>(n, 0)), pOcean(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++) q.push({i, 0});
+        for(int j = 0; j < n; j++) q.push({0, j});
+        bfs(q, heights, m, n, aOcean);
+
+        for(int i = 0; i < m; i++) q.push({i, n-1});
+        for(int j = 0; j < n; j++) q.push({m-1, j});
+        bfs(q, heights, m, n, pOcean);
+
+        vector<vector<int>> res;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(aOcean[i][j] == 1 && pOcean[i][j] == 1)
+                    res.push_back({i, j});
+            }
+        }
+        return res;
+    }
+
+    void bfs(queue<pair<int, int>>& q, vector<vector<int>>& heights, int m, int n, vector<vector<int>>& ocean) {
+        while(!q.empty()) {
+            int x = q.front().first, y = q.front().second;
+            q.pop();
+            ocean[x][y] = 1;
+
+            vector<vector<int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dirs[i][0], ny = y + dirs[i][1];
+                if(nx >= 0 && nx < m && ny >= 0 && ny < n && heights[nx][ny] >= heights[x][y] && ocean[nx][ny] == 0)
+                    q.push({nx, ny});
+            }
+        }
     }
 };
