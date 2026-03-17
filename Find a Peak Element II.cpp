d2e21e458c69ae83880b3502e016878374203cb8
Amit Dutta <amitdutta2406@gmail.com>
2026-03-17 13:26:37 +0000

    Refactor findPeakGrid function

--- Find a Peak Element II.cpp
+++ Find a Peak Element II.cpp
@@ -1,24 +1,34 @@
+// Best explanation: https://leetcode.com/problems/find-a-peak-element-ii/solutions/1276556/javapythonc-clear-explanation-with-image-au16/
+/*
+1. Get the max element from each column
+2. Do Peak Element I on each of those values
+3. But we don't need max element from each column, otherwise it would be m*n
+4. Take only midCol and disregard the left or right half. So binary search on the columns
+*/
+
+// O(m * logn)
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        
         int low = 0, high = n-1;
         while(low <= high) {
-            int midcol = low + (high-low)/2;
-            int maxrow = 0;
+            int midCol = low + (high-low)/2;
+            int maxRow = 0;
+
             for(int i = 0; i < m; i++) {
-                maxrow = mat[i][midcol] > mat[maxrow][midcol] ? i : maxrow; 
+                if(mat[i][midCol] > mat[maxRow][midCol])
+                    maxRow = i;
             }
-            bool isLeft = midcol-1 >= 0 && mat[maxrow][midcol-1] > mat[maxrow][midcol];
-            bool isRight = midcol+1 < n && mat[maxrow][midcol+1] > mat[maxrow][midcol];
-            if(!isLeft && !isRight) 
-                return {maxrow, midcol};
-            else if(isLeft)
-                high = midcol-1;
+            bool isLeftBigger = midCol - 1 >= 0 && mat[maxRow][midCol-1] > mat[maxRow][midCol];
+            bool isRightBigger = midCol + 1 < n && mat[maxRow][midCol+1] > mat[maxRow][midCol];
+            if(!isLeftBigger && !isRightBigger)
+                return {maxRow, midCol};
+            else if(isLeftBigger) 
+                high = midCol - 1;
             else
-                low = midcol+1;
+                low = midCol + 1;
         }
-        return {};
+        return {-1, -1};
     }
 };
